3340d70b0e95434bfc7919ff8aec1e558881cdc7
TerranDude <drago1257@gmail.com>
2022-04-15 17:06:19 +0300

    Add auth

--- crew_invest/app/views.py
+++ crew_invest/app/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from .models import *
 from .forms import *
 from django.shortcuts import redirect
-#from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.contrib.auth.decorators import login_required
 
 
-#@login_required
+@login_required
 def profile(request):
-    request.user = User.objects.get(pk=2)
 
     stocks = request.user.profile.stocks.order_by('-amount')
     return render(request, 'profile.html', {'stocks': stocks})
@@ -15,14 +17,13 @@ def profile(request):
 def main(request):
     return render(request, 'main.html')
 
-#@login_required
+@login_required
 def payment(request):
     return render(request, 'payment.html')
 
 
-#@login_required
+@login_required
 def pay(request):
-    request.user = User.objects.get(pk=2)
 
     form = PaymentForm(request.POST)
     if form.is_valid():
@@ -33,3 +34,62 @@ def pay(request):
         return render(request, 'payment.html', {'form': form })
 
     return redirect('profile')
+    
+def logout_view(request):
+    logout(request)
+    return redirect('main')
+    
+def signup(request):
+    if request.method == 'POST':
+        form = UserRegistration(request.POST)
+        if form.is_valid():
+            user=form.save()
+            profile=Profile(user=user,balance=0)
+            profile.id
+            profile.save()
+            login(request,user)
+            return redirect('main')
+    else:
+        form = UserRegistration()
+    return render(request,'signup.html', {'form':form})
+
+def auth_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username =form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=raw_password)
+            login(request,user)
+            return redirect('main')           
+    else:
+        form = AuthenticationForm(request.POST)
+    return render(request,'login.html',{'form':form})
+    
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user = request.user
+        profile = request.user.profile 
+        form = UserEdit(request.POST,request.FILES)
+        user.profile.avatar=form.instance
+        if form.is_valid():
+            user.username=request.POST.__getitem__('username')
+            user.email=request.POST.__getitem__('email')
+            user.first_name=request.POST.__getitem__('first_name')
+            user.last_name=request.POST.__getitem__('last_name')
+            profile = Profile.objects.get(pk=user.profile.id)
+            profile.avatar=form.cleaned_data.get('avatar_field')
+            profile.save()
+            user.save()
+            return redirect('profile')
+        else:
+            return redirect('main') 
+    else: 
+        form = UserEdit(request.POST)
+
+    return render(request,'edit.html',{'form':form}) 
+    
+    
+    
+    
